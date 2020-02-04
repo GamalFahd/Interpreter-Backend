178917857166b2f3aa5f360da3f7f5098f5565c9
Fahd GAMAL <fahd.gamal.info@gmail.com>
2020-02-05 00:15:10 +0100

    task 01 - Models creation

---
+++ src/main/java/com/sprintcodes/models/NotebookRequest.java
@@ -0,0 +1,21 @@
+package com.sprintcodes.models;
+
+public class NotebookRequest {
+	
+	private String code;
+
+	public NotebookRequest() {
+		super();
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/sprintcodes/models/NotebookResponse.java
@@ -0,0 +1,21 @@
+package com.sprintcodes.models;
+
+public class NotebookResponse {
+	
+	private String response;
+
+	public NotebookResponse() {
+		super();
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+	
+	
+
+}
