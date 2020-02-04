ac7c8447da9ab84b82df09268bc3cf8bfb3068b6
Fahd GAMAL <fahd.gamal.info@gmail.com>
2020-02-04 23:34:03 +0100

    [Conf] - Project init

---
+++ src/test/java/com/sprintcodes/InterpreterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintcodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterpreterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
